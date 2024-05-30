eb77fe1cad2f7ccd693ee1d1cd67568a0a6aeb74
hexaalif <shadullahsad9@gmail.com>
2024-05-30 20:30:29 +0600

    both local and host link is added to url

--- src/hooks/useTasks.jsx
+++ src/hooks/useTasks.jsx
@@ -4,21 +4,27 @@ import { useEffect, useState } from "react";
 const useTasks = () => {
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(true);
+  const urls = [
+    "http://localhost:8000/todo/list/",
+    "https://work-notes-server.onrender.com/todo/list/",
+  ];
 
   useEffect(() => {
     const getTasks = async () => {
-      try {
-        const res = await axios.get("http://localhost:8000/todo/list/", {
-          // headers: {
-          //   Authorization: `token ${localStorage.getItem("token")}`,
-          // },
-        });
-        console.log(res.data);
-        setTasks(res.data);
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setLoading(false);
+      for (const url of urls) {
+        try {
+          const res = await axios.get(url, {
+            // headers: {
+            //   Authorization: `token ${localStorage.getItem("token")}`,
+            // },
+          });
+          console.log(res.data);
+          setTasks(res.data);
+        } catch (err) {
+          console.log(err);
+        } finally {
+          setLoading(false);
+        }
       }
     };
     getTasks();

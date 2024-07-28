b7bfbcabe6f1e140d719df593c4cf71707ba02ba
hexaalif <shadullahsad9@gmail.com>
2024-07-28 13:08:02 +0600

    react loader spinner added to profile

--- src/component/pages/Profile/Profile.jsx
+++ src/component/pages/Profile/Profile.jsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 import useTasks from "../../../hooks/useTasks";
 import useUsers from "../../../hooks/useUsers";
 import CountUp from "react-countup";
+import { Audio } from "react-loader-spinner";
+// import { BsTextCenter } from "react-icons/bs";
 
 const Profile = () => {
-  const [users1] = useUsers();
+  const [users1, loading] = useUsers();
   const [tasks] = useTasks();
 
   const completedtask = tasks.filter(
@@ -23,82 +25,100 @@ const Profile = () => {
 
   return (
     <div className="bg-gray-800 text-white">
-      <div className="block md:flex p-6 md:p-12">
-        <div className="bg-gray-700 w-full md:w-1/3 p-3 md:p-6 rounded-lg">
-          <img
-            src=""
-            className="h-44 md:h-64 w-full border-2 rounded-lg"
-            alt="#"
-          />
-          <div className="mt-3 md:mt-6">
-            <div className="block md:flex items-center justify-between">
-              <h1 className="text-xl md:text-3xl">My Profile</h1>
-              <div className="block md:flex items-center bg-gray-800 px-3 py-2 rounded-lg">
-                <p>Active Now </p>
-                <div className="bg-green-600 h-3 w-3 rounded-full ml-1"></div>
+      <div className="block md:flex justify-center items-center p-6 md:p-12">
+        {loading ? (
+          <>
+            <div className="flex justify-center items-center h-screen">
+              <Audio
+                height="80"
+                width="80"
+                radius="9"
+                color="gray"
+                ariaLabel="three-dots-loading"
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="bg-gray-700 w-full md:w-1/3 p-3 md:p-6 rounded-lg">
+              <img
+                src=""
+                className="h-44 md:h-64 w-full border-2 rounded-lg"
+                alt="#"
+              />
+              <div className="mt-3 md:mt-6">
+                <div className="block md:flex items-center justify-between">
+                  <h1 className="text-xl md:text-3xl">My Profile</h1>
+                  <div className="block md:flex items-center bg-gray-800 px-3 py-2 rounded-lg">
+                    <p>Active Now </p>
+                    <div className="bg-green-600 h-3 w-3 rounded-full ml-1"></div>
+                  </div>
+                </div>
+
+                <div className="flex justify-between">
+                  <p className="underline my-3">
+                    {users1.first_name} {users1.last_name}
+                  </p>
+                  <p className="text-xl mt-3 underline">
+                    Username:{" "}
+                    <span className="text-white">{users1.username}</span>
+                  </p>
+                </div>
+                <p className="underline">Email: {users1.email}</p>
+              </div>
+              <div className="text-center mt-3">
+                <button className="py-2 px-3 rounded-lg bg-cyan-500">
+                  Edit Profile
+                </button>
               </div>
             </div>
 
-            <div className="flex justify-between">
-              <p className="underline my-3">
-                {users1.first_name} {users1.last_name}
-              </p>
-              <p className="text-xl mt-3 underline">
-                Username: <span className="text-white">{users1.username}</span>
-              </p>
-            </div>
-            <p className="underline">Email: {users1.email}</p>
-          </div>
-          <div className="text-center mt-3">
-            <button className="py-2 px-3 rounded-lg bg-cyan-500">
-              Edit Profile
-            </button>
-          </div>
-        </div>
-        <div className="w-full">
-          <div>
-            <h1 className="text-3xl m-6">Task Progress</h1>
-            <div className="m-12 grid grid-cols-1 md:grid-cols-3 gap-6">
-              <div className="bg-green-400 p-2 md:p-4 rounded-md">
-                <h4 className="text-xs md:text-xl mb-3">Completed</h4>
-                <p>
-                  <span className="text-sm md:text-4xl">
-                    <CountUp start={0} end={completedtask} duration={5} />{" "}
-                  </span>{" "}
-                  Tasks
-                </p>
+            <div className="w-full">
+              <div>
+                <h1 className="text-3xl m-6">Task Progress</h1>
+                <div className="m-12 grid grid-cols-1 md:grid-cols-3 gap-6">
+                  <div className="bg-green-400 p-2 md:p-4 rounded-md">
+                    <h4 className="text-xs md:text-xl mb-3">Completed</h4>
+                    <p>
+                      <span className="text-sm md:text-4xl">
+                        <CountUp start={0} end={completedtask} duration={5} />{" "}
+                      </span>{" "}
+                      Tasks
+                    </p>
+                  </div>
+                  <div className="bg-cyan-400 p-2 md:p-4 rounded-md">
+                    <h4 className="text-xs md:text-xl mb-3">In progress</h4>
+                    <p>
+                      <span className="text-sm md:text-4xl">
+                        <CountUp start={0} end={inProgress} duration={5} />{" "}
+                      </span>{" "}
+                      Tasks
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div className="bg-cyan-400 p-2 md:p-4 rounded-md">
-                <h4 className="text-xs md:text-xl mb-3">In progress</h4>
-                <p>
-                  <span className="text-sm md:text-4xl">
-                    <CountUp start={0} end={inProgress} duration={5} />{" "}
-                  </span>{" "}
-                  Tasks
-                </p>
+              <div>
+                <h1 className="text-3xl m-6">My Tasks</h1>
+                <div className="bg-gray-600 mx-3 md:mx-12 p-3 md:p-6 rounded-lg">
+                  {tasks
+                    .filter((task) => task.user.id == users1.id)
+                    .map((task) => (
+                      <>
+                        <div className="flex justify-between items-center m-3 p-2 bg-indigo-600 rounded-lg">
+                          <p className="font-bold">{task.title}</p>
+                          <Link to={`/${task.id}`}>
+                            <button className="bg-lime-600 p-2 rounded-md">
+                              View
+                            </button>
+                          </Link>
+                        </div>
+                      </>
+                    ))}
+                </div>
               </div>
             </div>
-          </div>
-          <div>
-            <h1 className="text-3xl m-6">My Tasks</h1>
-            <div className="bg-gray-600 mx-3 md:mx-12 p-3 md:p-6 rounded-lg">
-              {tasks
-                .filter((task) => task.user.id == users1.id)
-                .map((task) => (
-                  <>
-                    <div className="flex justify-between items-center m-3 p-2 bg-indigo-600 rounded-lg">
-                      <p className="font-bold">{task.title}</p>
-                      <Link to={`/${task.id}`}>
-                        <button className="bg-lime-600 p-2 rounded-md">
-                          View
-                        </button>
-                      </Link>
-                    </div>
-                  </>
-                ))}
-            </div>
-          </div>
-        </div>
+          </>
+        )}
       </div>
     </div>
   );
